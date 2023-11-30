3c691cba9ca2075f0a20605fb6586f701a2a313b
S567536 <S567536@nwmissouri.edu>
2023-11-29 23:12:31 -0600

    added secuity

--- models/phone.js
+++ models/phone.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose")
 const phoneSchema = mongoose.Schema({
     Phone_Name:String,
-    Phone_price:Number,
-    Phone_Storage:Number
+    Phone_price:{
+        type: Number,
+        min:10000,
+        max: 1000000},
+    Phone_Storage:{
+        type: Number,
+        min:128,
+        max: 1024}
 })
 module.exports = mongoose.model("phone",phoneSchema)
 
--- routes/phone.js
+++ routes/phone.js
@@ -1,6 +1,7 @@
 var express = require('express');
     const phone_controlers= require('../controllers/phone');
     var router = express.Router();
+
     /* GET phone */
     router.get('/', phone_controlers.phone_view_all_Page );
     // GET request for one phone.
@@ -8,8 +9,6 @@ var express = require('express');
     module.exports = router;
     /* GET detail phone page */
     router.get('/detail', phone_controlers.phone_view_one_Page);
-    /* GET create phone page */
-    router.get('/create', phone_controlers.phone_create_Page);
     /* GET create update page */
     const secured = (req, res, next) => {
         if (req.user){
@@ -19,7 +18,9 @@ var express = require('express');
         }
     router.get('/update',secured, phone_controlers.phone_update_Page);
     /* GET delete phone page */
-    router.get('/delete', phone_controlers.phone_delete_Page);
+    router.get('/delete',secured, phone_controlers.phone_delete_Page);
+    /* GET create phone page */
+    router.get('/create',secured, phone_controlers.phone_create_Page);
 
 
 
